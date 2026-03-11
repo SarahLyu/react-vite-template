0ce2adb464a4fcc2420eb5440eeb831b0af7d254
Sarah Lyu <sarah.lyu2012@gmail.com>
2026-03-11 15:28:03 +0800

    Refactor ErrorPage component export style

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -2,11 +2,9 @@ import { useRouteError } from 'react-router';
 
 import ErrorFallback from './components/ErrorFallback';
 
-function ErrorPage() {
+export default function ErrorPage() {
   const error = useRouteError() as any;
   console.error('路由错误：', error);
 
   return <ErrorFallback error={error} resetErrorBoundary={() => window.location.reload()} />;
 }
-
-export default ErrorPage;
